8c9fc80e30c86da8ec6908b6bac7c31e1934614d
Leonid <leonidchernobylskiy@gmail.com>
2021-06-02 23:34:06 +0300

    add course

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const Handlebars = require('handlebars');
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+  try {
+    const url = `mongodb+srv://leonid0991:ZqbTnTtJHZnA1B5U@cluster0.44zzq.mongodb.net/shop`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start()
+
+const password = 'ZqbTnTtJHZnA1B5U'
